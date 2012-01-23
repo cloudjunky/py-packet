79aa76358f0b5dfea63074dfa8f08c9646c484e2
Michael Baker <cloudjunky@gmail.com>
2012-01-23 15:32:05 +1100

    Finished everything except the quirks.

--- packet.py
+++ packet.py
@@ -3,7 +3,7 @@ from impacket.ImpactDecoder import EthDecoder
 from impacket.ImpactPacket import IP, TCP, UDP, ICMP
 from impacket.ImpactPacket import TCPOption
 
-pcap = open_offline("/home/michael/Downloads/SlowchopCaptures/0806.cap")
+pcap = open_offline("/Users/michaelb/Downloads/SlowchopCaptures/small3.cap")
 
 decoder = EthDecoder()
 metrics = {}
@@ -14,6 +14,7 @@ def callback(hdr,data):
     if isinstance(l2,IP):
         src_ip = l2.get_ip_src()
         dst_ip = l2.get_ip_dst()
+        ip_version = l2.get_ip_v() 
         ip_ttl = l2.get_ip_ttl()
         ip_df = l2.get_ip_df() #16 bit field outputs 16384 if set
         ip_hl = l2.get_ip_hl()
@@ -30,11 +31,19 @@ def callback(hdr,data):
               tcp_size = l3.get_size()
               tcp_syn_bit = l3.get_flag(2)
 
+              signature = []
+              #Set features to zero incase they are not set in the packet
+              tcp_mss=0
+              tcp_window=0
+              tcp_scale=0
+              tcp_olen=0
+
               option_layout = []
               option_numeric = []
+
               for option in l3.get_options():
                    option_numeric.append(option.get_kind())
-
+                   tcp_olen = option.get_size()
                    if option.get_kind() == TCPOption.TCPOPT_EOL:
                        #TCP Option 0
                        option_layout.append('eol')
@@ -51,7 +60,7 @@ def callback(hdr,data):
                    elif option.get_kind() ==TCPOption.TCPOPT_WINDOW:
                        #TCP Option 3 - Window Scale
                        option_layout.append('scale')
-                       #print option.get_len()
+                       tcp_scale = option.get_shift_cnt()
 
                    elif option.get_kind() == TCPOption.TCPOPT_SACK_PERMITTED:
                        #TCP Option 4
@@ -70,26 +79,26 @@ def callback(hdr,data):
                        #TCP Option 19
                        option.layout.append('sig')
                        print "sig"
-
-                   #Finish catch-all and add option number e.g. n?
-
+               
+              #Finish catch-all and add option number e.g. n?
               #Create dictionary for ip address and add metrics.
               # If the key exists don't add the metric
               # Print all IP's and metrics.
 
+              signature.append(ip_version)
+              signature.append(ip_ttl)
+              signature.append(tcp_olen)
+              signature.append(ip_hl)
+              signature.append(tcp_mss)
+              signature.append(tcp_window)
+              signature.append(tcp_scale)
+              signature.append(option_layout)
+
+
               if src_ip in metrics:
                  pass
               else:
-                  metrics[src_ip] = "%s,%s" % (tcp_window,ip_ttl)
-
-              #print "%s -> %s(%s)" % (src_ip, dst_ip, tcp_dst_port)
-              #print "TTL:%s, DF:%s, Header Length: %s" % (ip_ttl, ip_df, ip_hl)
-              #print "Seq: %s, Flags: %s, Window: %s" % (tcp_seq, tcp_flags,
-              #        tcp_window)
-              #print "Header Size: %s, Size: %s" % (tcp_header_size, tcp_size)
-              #print "Options: %s" % option_layout
-              #print "Options Numeric:" % option_numeric
-              #print "MSS: %s:" % tcp_mss
+                  metrics[src_ip] = signature 
 
 
 pcap.loop(0,callback)
